8c396e2b1958420a5e25b3c12b5354f223652132
Kehinde Aminat <aimenah@outlook.com>
2023-09-28 23:40:52 +0100

    Update schema.sql to create owners

--- schema.sql
+++ schema.sql
@@ -8,3 +8,15 @@ CREATE TABLE ANIMALS (
    NEUTERED BOOLEAN,
    WEIGHT_KG NUMERIC
 );
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+ALTER TABLE animals
+    ADD COLUMN species_id INTEGER REFERENCES species(id),
+    ADD COLUMN owner_id INTEGER REFERENCES owners(id);
